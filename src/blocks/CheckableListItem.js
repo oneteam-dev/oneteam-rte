de3eab73e1855831790ec49394bb748e78cda68f
sugarshin <shinsugar@gmail.com>
2016-04-21 18:14:31 +0900

    fix behavior of when you move the cursor in the checkbox list

--- src/blocks/CheckableListItem.js
+++ src/blocks/CheckableListItem.js
@@ -12,14 +12,13 @@ export default class CheckableListItem extends Component {
 
     return (
       <div data-offset-key={offsetKey} className='checkable-list-item-body'>
-        <div className={classNames('checkable-list-item-checkbox', {
-          'is-checked': checked
-        })}>
-          <input
-            type='checkbox'
-            contentEditable='false'
-            suppressContentEditableWarning
-            checked={checked} onChange={onChangeChecked} />
+        <div
+          className={classNames('checkable-list-item-checkbox', {
+            'is-checked': checked
+          })}
+          contentEditable='false'
+          suppressContentEditableWarning>
+          <input type='checkbox' checked={checked} onChange={onChangeChecked} />
         </div>
         <div className='checkable-list-item-text'>
           <EditorBlock {...this.props} />
