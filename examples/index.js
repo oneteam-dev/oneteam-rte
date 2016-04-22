a10255db4610d9d624db51483f170480a63840b5
sugarshin <shinsugar@gmail.com>
2016-04-22 11:52:13 +0900

    update examples

--- examples/index.js
+++ examples/index.js
@@ -2,23 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import RichTextEditor from '../lib';
 import '../lib/base.css';
-import './index.css';
+import './index.styl';
 
 let richTextEditor;
 
 ReactDOM.render((
   <div>
     <RichTextEditor
-      placeholder='placeholder'
-      readOnly={false}
       ref={c => richTextEditor = c}
       onEnterKeyDownWithCommand={() => console.log('onEnterKeyDownWithCommand')}
       onPaste={() => console.log('onPaste')}
       onClickAddImage={() => console.log('onClickAddImage')}
-      onClickFileAttach={() => console.log('onClickFileAttach')}
-      initialHtml={'<h1>RichTextEditor</h1>'}
-      headingLabel='Heading'
-      useDefaultButtons />
-    <buttton onClick={() => console.log(richTextEditor.getHTML())}>Log</buttton>
+      onClickFileAttach={() => console.log('onClickFileAttach')} />
+    <button onClick={() => console.log(richTextEditor.getHTML())}>Log</button>
   </div>
 ), document.querySelector('#app-root'));
