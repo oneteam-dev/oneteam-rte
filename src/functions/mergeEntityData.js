b7bbbe552c6e6594ce19e4b822116a4407b54b7a
Shingo Sato <shinsugar@gmail.com>
2016-11-24 20:08:17 +0900

    Fix `mergeEntityData`

--- src/functions/mergeEntityData.js
+++ src/functions/mergeEntityData.js
@@ -2,13 +2,14 @@ import { EditorState, Entity } from 'draft-js';
 
 /**
  * @param {EditorState} editorState
- * @param {String} entityKey
- * @param {} data
- * @returns {EditorState} New editorState.
+ * @param {string} entityKey
+ * @param {*} data
+ * @returns {EditorState} Next `editorState`
  */
 export default function mergeEntityData(editorState, entityKey, data) {
   Entity.mergeData(entityKey, data);
   // `Entity.mergeData` does not mutate contentState in any way
   // https://github.com/facebook/draft-js/issues/399
-  return EditorState.forceSelection(editorState, editorState.getSelection());
+  // `EditorState.forceSelection` will be forcibly focus on the editor
+  return EditorState.createWithContent(editorState.getCurrentContent(), editorState.getDecorator());
 }
