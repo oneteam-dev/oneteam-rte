32621138131c117b1757b15af88fd095232de3d6
sugarshin <shinsugar@gmail.com>
2016-12-15 22:12:26 +0900

    Load Prism languages all

--- src/plugins/prism.js
+++ src/plugins/prism.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import Prism from 'prismjs';
 import PrismDecorator from 'draft-js-prism';
-import 'prismjs/components/prism-java';
-import 'prismjs/components/prism-scala';
-import 'prismjs/components/prism-go';
-import 'prismjs/components/prism-sql';
-import 'prismjs/components/prism-bash';
-import 'prismjs/components/prism-c';
-import 'prismjs/components/prism-cpp';
-import 'prismjs/components/prism-kotlin';
-import 'prismjs/components/prism-perl';
-import 'prismjs/components/prism-ruby';
-import 'prismjs/components/prism-swift';
+import './prism-languages';
 
 const prismPlugin = {
   decorators: [
