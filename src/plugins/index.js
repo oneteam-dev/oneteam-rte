60675c358099edb38081c9826404b0e7d80bcd3c
sugarshin <shinsugar@gmail.com>
2017-04-10 18:30:06 +0900

    Emoji suggestion picker

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,10 +1,13 @@
 import merge from 'lodash/merge';
+import { emojioneList } from 'emojione';
 import createOneteamRTEPlugin from 'draft-js-oneteam-rte-plugin';
 import createLinkifyPlugin from 'draft-js-linkify-plugin';
 import createBlockBreakoutPlugin from 'draft-js-block-breakout-plugin';
 import createMarkdownShortcutsPlugin from 'draft-js-markdown-shortcuts-plugin';
 import createCheckableListPlugin from 'draft-js-checkable-list-plugin';
+import createEmojiPlugin from 'draft-js-emoji-plugin';
 import prismPlugin from './prism';
+import createEmojiListWithOutHighPriorityList from './helpers/createEmojiListWithOutHighPriorityList';
 
 const defaultConfig = {
   oneteamRTE: {},
@@ -15,16 +18,27 @@ const defaultConfig = {
   },
   markdownShortcuts: {},
   checkableList: { sameWrapperAsUnorderedListItem: true },
-  linkifyPlugin: {}
+  linkify: {},
+  emoji: {
+    highPriorityShortnames: [],
+    priorityList: Object.keys(emojioneList)
+      .filter(key => !/_tone[0-9]$/.test(key)) // without tones
+      .reduce((ret, key) => ({ ...ret, [key]: emojioneList[key].unicode }), {})
+  }
 };
 
 const createPlugins = (configs) => {
   const config = merge({}, defaultConfig, configs);
+  config.emoji.priorityList = createEmojiListWithOutHighPriorityList(
+    config.emoji.highPriorityShortnames,
+    config.emoji.priorityList
+  );
   const oneteamRTEPlugin = createOneteamRTEPlugin(config.oneteamRTE);
   const blockBreakoutPlugin = createBlockBreakoutPlugin(config.blockBreakout);
   const markdownShortcutsPlugin = createMarkdownShortcutsPlugin(config.markdownShortcuts);
   const checkableListPlugin = createCheckableListPlugin(config.checkableList);
-  const linkifyPlugin = createLinkifyPlugin(config.linkifyPlugin);
+  const linkifyPlugin = createLinkifyPlugin(config.linkify);
+  const emojiPlugin = createEmojiPlugin(config.emoji);
 
   // TODO: It becomes a block when pasting html containing `<code>`
   // https://github.com/ngs/draft-js-markdown-shortcuts-plugin/blob/28c6479ec137146a1bf8dedac6fae42c651fcf43/src/index.js#L24
@@ -39,9 +53,11 @@ const createPlugins = (configs) => {
       markdownShortcutsPlugin,
       checkableListPlugin,
       linkifyPlugin,
-      prismPlugin
+      prismPlugin,
+      emojiPlugin
     ],
-    oneteamRTEPlugin
+    oneteamRTEPlugin,
+    emojiPlugin
   };
 }
 
