ca0876a1619663b370e643b8d00747e39e355f2d
sugarshin <shinsugar@gmail.com>
2018-09-18 22:17:13 +0900

    Fix script

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -6,6 +6,7 @@ TAG=$(git describe --abbrev=0 --tags 2>/dev/null)
 VERSION=${TAG:1}
 if [[ $VERSION && -z "$(npm info "${PKG}@${VERSION}" --json)" ]]; then
   echo $VERSION
+  NODE_ENV=production npm run build
   npm publish
 else
   echo "${VERSION} exists. It was skip publishing."
