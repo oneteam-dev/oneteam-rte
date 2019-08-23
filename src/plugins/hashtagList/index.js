fec05ae45e15e4e4c366255829c0cf4b6da9e5c6
Hitoshi Kato <12182520+qatoco@users.noreply.github.com>
2019-08-23 23:10:26 +0900

    remove css file from dependency

--- src/plugins/hashtagList/index.js
+++ src/plugins/hashtagList/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import createPluginObject, { defaultTheme } from 'draft-js-mention-plugin';
-import './style.css';
 
 /**
  * convert string[] into { id: string; name: string;  userName: string; email: string;}[]
@@ -10,16 +9,21 @@ import './style.css';
 export const convertToMentions = (hashtagList = []) =>
   hashtagList.map((name, i) => ({ id: `hashtag-item-${i}`, name, userName: '', email: '' }));
 
+const defaultStyle = { fontWeight: 'bold', background: 'transparent' };
+
 const MentionComponent = ({ children, theme }) => (
-  <span className={classnames(theme.mention, theme.hashtag)}>{children}</span>
+  <span className={classnames(theme.mention, theme.custom)} style={defaultStyle}>
+    {children}
+  </span>
 );
+
 MentionComponent.propTypes = { children: PropTypes.array, theme: PropTypes.object };
 
 // TODO: replace with a dedicated hashtag suggestion plugin which has no dependency on 'draft-js-mention-plugin'
 // Ad hoc support for hashtag suggest on rich text editor
 export default hashtagList =>
   createPluginObject({
-    theme: { ...defaultTheme, hashtag: 'draft-js-mention-plugin-hashtag' },
+    theme: { ...defaultTheme, custom: 'draft-js-mention-plugin-hashtag-custom' },
     mentionPrefix: '',
     mentionTrigger: '#',
     mentions: convertToMentions(hashtagList),
