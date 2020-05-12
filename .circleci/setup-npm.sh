3945ecfcc55c879b0a2bf038133ab33d5eec80c9
Hitoshi Kato <12182520+qatoco@users.noreply.github.com>
2020-05-12 14:26:42 +0900

    change npm user

--- .circleci/setup-npm.sh
+++ .circleci/setup-npm.sh
@@ -4,4 +4,4 @@ cat > ~/.npmrc << EOF
 //registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN
 EOF
 
-[[ $(npm whoami) == "sugarshin" ]] || sh -c "echo not\\ authenticated; exit 1"
+[[ $(npm whoami) == "kazsakai" ]] || sh -c "echo not\\ authenticated; exit 1"
